c5a64a2de03b32f9dec3800890f87d65094e5c4a
Bruce Lay <bruce.lay@secureflag.com>
2022-03-18 16:55:23 +0000

    HotspotItem tests

---
+++ tests/components/HotspotItem.test.js
@@ -0,0 +1,37 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ import React from 'react'
+ import {mount, shallow} from 'enzyme'
+ 
+ import HotspotItem from '../../src/main/js/knowledge_base/components/HotspotItem';
+ 
+ test('HotspotItem creates link to issue', () => {
+   const hotspot = {
+     project: "project",
+     key: "key"
+   }
+   const wrapper = shallow(<HotspotItem hotspot={hotspot}/>);
+   expect(wrapper.debug()).toContain("<a href=\"/security_hotspots?id=project&hotspots=key\" />");
+ });
+ 
+ test('HotspotItem extracts exrLink', () => {
+   const hotspot = {
+     project: "project",
+     key: "key",
+     kb: "Click [here](https://exrlink.com)"
+   }
+   const wrapper = mount(<HotspotItem hotspot={hotspot}/>);
+   expect(wrapper.debug()).toContain('href="https://exrlink.com"');
+ });
+
+ test('HotspotItem "N/A" kb', () => {
+  const hotspot = {
+    project: "project",
+    key: "key",
+    kb: "N/A"
+  }
+  const wrapper = mount(<HotspotItem hotspot={hotspot}/>);
+  expect(wrapper.debug()).toContain('N/A');
+ });
