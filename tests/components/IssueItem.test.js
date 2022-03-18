a1b0b815aaaf73050b4040d859da049beff7e811
Bruce Lay <bruce.lay@secureflag.com>
2022-03-18 16:07:22 +0000

    IssueItem tests

---
+++ tests/components/IssueItem.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react'
+import {mount, shallow} from 'enzyme'
+
+import IssueItem from '../../src/main/js/knowledge_base/components/IssueItem';
+
+test('IssueItem creates link to issue', () => {
+  const issue = {
+    project: "project",
+    key: "key"
+  }
+  const wrapper = shallow(<IssueItem issue={issue}/>);
+  expect(wrapper.debug()).toContain('<a href="/project/issues?id=project&open=key" />');
+});
+
+test('IssueItem extracts exrLink', () => {
+  const issue = {
+    project: "project",
+    key: "key",
+    kb: "Click [here](https://exrlink.com)"
+  }
+  const wrapper = mount(<IssueItem issue={issue}/>);
+  expect(wrapper.debug()).toContain('href="https://exrlink.com"');
+});
