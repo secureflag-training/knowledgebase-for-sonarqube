1a835e964207c6152c891b1eef77f3149292ba44
Bruce Lay <bruce.lay@secureflag.com>
2022-03-22 14:21:44 +0000

    Enzyme adapter in setting up test environment

--- conf/jest/SetupTestEnvironment.js
+++ conf/jest/SetupTestEnvironment.js
@@ -17,6 +17,11 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 window.baseUrl = '';
 
 window.t = (window.tp = function() {
