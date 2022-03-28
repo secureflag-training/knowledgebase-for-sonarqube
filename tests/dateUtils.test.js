2d0cda3d403cb85167ebeb74567dc00dbf13ad89
Bruce Lay <bruce.lay@secureflag.com>
2022-03-28 15:50:40 +0100

    Mock timeZone in dateUtils test

--- tests/dateUtils.test.js
+++ tests/dateUtils.test.js
@@ -5,6 +5,11 @@
 import { formatDateWithBrowserTimeZone } from "../src/main/js/common/dateUtils";
 
 test('Timezone is formatted correctly', () => {
+  jest.spyOn(Intl, 'DateTimeFormat').mockImplementation(() => ({
+    resolvedOptions: () => ({
+      timeZone: 'Europe/London'
+    })
+  }));
   const dateString = formatDateWithBrowserTimeZone(new Date('2022-02-24T16:39:37+0000'));
   expect(dateString).toEqual('24/02/2022, 16:39:37');
 });
